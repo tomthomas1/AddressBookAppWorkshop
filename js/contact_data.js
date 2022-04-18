4ce20d8f70ca183dd334ac19eac06cbfeda5c116
tomthomas1 <tomthomas8688@gmail.com>
2022-04-19 01:26:24 +0530

    After submit create Json object

--- js/contact_data.js
+++ js/contact_data.js
@@ -1,6 +1,6 @@
 const nameRegex = RegExp("^[A-Z]{1}[A-Za-z\\s]{2,}$");
 const phoneNumberRegex = RegExp("^[0-9]{2}\\s{1}[7-9]{1}[0-9]{9}$");
-const addressRegex = RegExp('^[a-zA-Z0-9#,&]{4,}$');
+const addressRegex = RegExp('^[a-zA-Z0-9#,&\\s]{4,}$');
 const zipRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
 
 class Contact {
